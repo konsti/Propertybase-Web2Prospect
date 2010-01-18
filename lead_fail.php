74ab856a41846c5d4a305aeea9f0d78a04596c22
Konstantin Krauss <konstantin@propertybase.com>
2010-01-18 17:51:04 +0100

    Add Comments

--- lead_fail.php
+++ lead_fail.php
@@ -4,6 +4,7 @@
 </head>
 <body>
 	Lead Failure
+	<!-- This page can also be pure HTML. Use PHP or any other script language to get the error message. -->
 	<pre><?php print_r($_GET); ?></pre>
 </body>
 </html>
